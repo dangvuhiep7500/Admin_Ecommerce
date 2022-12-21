33535621f1f89cb275d561cc7a89af821d4d9743
dangvuhiep7500 <dangvuhiep1291@gmail.com>
2022-12-21 22:40:02 +0700

    update

---
+++ Admin_Ecommerce/Controller API/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Products
+{
+    public string? _id { get; set; }
+
+    [Required(ErrorMessage = "title is required")]
+    public string? title { get; set; }
+
+    [Required(ErrorMessage = "description is required")]
+    public string? description { get; set; }
+
+    [Required(ErrorMessage = "image is required")]
+    public string? image { get; set; }
+    public List<string> imageDetail { get; set; }
+
+    [Required(ErrorMessage = "categoryId is required")]
+    public string? categoryId { get; set; }
+
+    [Required(ErrorMessage = "price is required")]
+    public int price { get; set; }
+}
--- Admin_Ecommerce/Program.cs
+++ Admin_Ecommerce/Program.cs
@@ -1,16 +1,24 @@
 using Admin_Ecommerce;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
+using MudBlazor.Services;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+var app = builder.Build();
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
 });
-
+builder.Services.AddScoped(sp =>
+    new HttpClient
+    {
+        BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+    });
 await builder.Build().RunAsync();
+
+
