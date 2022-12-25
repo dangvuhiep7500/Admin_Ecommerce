ef24718df7899f396f94b6de6b5732dd1ed28e2e
dangvuhiep7500 <dangvuhiep1291@gmail.com>
2022-12-25 18:24:40 +0700

    update

--- Admin_Ecommerce/Program.cs
+++ Admin_Ecommerce/Program.cs
@@ -4,12 +4,10 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 var app = builder.Build();
-
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
 builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
