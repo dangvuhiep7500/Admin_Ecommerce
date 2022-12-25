a6e71e81a648a28e71538a63ae1abdb581fd16c7
dangvuhiep7500 <dangvuhiep1291@gmail.com>
2022-12-25 22:17:26 +0700

    update

---
+++ Admin_Ecommerce/Controller API/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+    public class User
+    {
+    public int _id { get; set; }
+    [Required]
+    public string email { get; set; }
+    [Required]
+    public string password { get; set; }
+    [Required]
+    public string firstName { get; set; }
+    [Required]
+    public string lastName { get; set; }
+    [Required]
+    public string phoneNumber { get; set; }
+    public string role { get; set; }
+
+    public string AccessToken { get; set; }
+    public string RefreshToken { get; set; }
+}
+
